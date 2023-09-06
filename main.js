e29719a88e3a2ba65adc2f8ddf694a8d5a010713
Чувахин Иван Юрьевич <chuvakhin@hopper-it.ru>
2023-09-06 10:19:39 +0300

    0.1.0

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const {Main, MenuItem} = require('chuijs');
+let json = require("./package.json");
 const main = new Main({
-    name: "Media Maze",
+    name: `${json.productName} (${json.version})`,
     //width: '25%',
     //height: '25%',
     icon: `${__dirname}/resources/icons/app/icon.png`,
@@ -9,7 +10,11 @@ const main = new Main({
 });
 
 main.start({
+    hideOnClose: true,
     tray: [
+        new MenuItem().separator(),
+        new MenuItem().help(`Версия: ${require("./package.json").version}`),
+        new MenuItem().separator(),
         new MenuItem().button('Показать | Скрыть', () => main.hideAndShow()),
         new MenuItem().button('Консоль', () => main.toggleDevTools()),
         new MenuItem().quit('Выход')
