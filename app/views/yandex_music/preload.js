3a21f64df87ce7d6cde9921221bcd9deac84a80b
syzoth <saintdizen@gmail.com>
2023-10-16 12:28:13 +0300

    0.0.6

--- app/views/yandex_music/preload.js
+++ app/views/yandex_music/preload.js
@@ -1,40 +1,41 @@
 //Mu.blocks.di.repo.player.play();
+//document.querySelector("body > div.page-root.page-root_no-player.deco-pane-back.theme.theme_dark.black > div.head-container > div > div > div.head-kids__left").remove();
+//document.querySelector("body > div.page-root.page-root_no-player.deco-pane-back.theme.theme_dark.black > div.centerblock-wrapper.deco-pane.theme.theme_dark.black > div.footer").remove();
 class Test {
-    #span1 = document.createElement("span");
-    #span2 = document.createElement("a");
+    #div1 = document.createElement("div");
+    #div2 = document.createElement("div");
+    #span1 = document.createElement("span")
     constructor() {
-        document.querySelector("body > div.page-root.page-root_no-player.deco-pane-back.theme.theme_dark.black > div.head-container > div > div > div.head-kids__left").remove();
-        document.querySelector("body > div.page-root.page-root_no-player.deco-pane-back.theme.theme_dark.black > div.centerblock-wrapper.deco-pane.theme.theme_dark.black > div.footer").remove();
-        //
-        this.#span1.className = "player-controls__btn-cast";
-        this.#span2.className = "d-icon deco-icon d-icon_share"
-        this.#span2.style.rotate = "180deg"
-        this.#span1.appendChild(this.#span2)
-        this.#span1.addEventListener("click", async () => {
-            //let link = JSON.parse(Mu.blocks.di.repo.player.getTrack()._$f9);
-            //this.#span2.setAttribute("href", link)
-            //this.#span2.setAttribute("download", Mu.blocks.di.repo.player.getTrack()._$f9)
-            let fileHandle = await showSaveFilePicker({
-                types: [{
-                    description: 'file',
-                }],
-            });
-            const writableStream = await fileHandle.createWritable()
-            await writableStream.write(Mu.blocks.di.repo.player.getTrack()._$f9)
-            // данный метод не упоминается в черновике спецификации,
-            // хотя там говорится о необходимости закрытия потока
-            // для успешной записи файла
-            await writableStream.close()
+        this.#div1.className = "hq";
+        this.#div2.className = "hq__icon player-controls__btn deco-player-controls__button"
+        this.#span1.className = "d-icon deco-icon d-icon_share"
+        this.#span1.style.rotate = "180deg"
+        this.#div1.addEventListener("click", async (evt) => {
+            let test_link = document.createElement("a");
+            let test = await fetch(Mu.blocks.di.repo.player.getTrack()._$f9);
+            const uri = window.URL.createObjectURL(await test.blob());
+            let artist = Mu.blocks.di.repo.player.getTrack().artists[0].name;
+            let title = Mu.blocks.di.repo.player.getTrack().title;
+            test_link.href = uri
+            test_link.download = `${artist} - ${title}.mp3`;
+            test_link.target = '_blank'
+            //
+            this.#div1.appendChild(test_link)
+            test_link.click()
+            test_link.remove()
+            evt.preventDefault()
         })
-        let test2 = document.querySelector("body > div.page-root.page-root_no-player.deco-pane-back.theme.theme_dark.black > div.bar > div.bar__content > div.player-controls.deco-player-controls > div.player-controls__track-container > div > div");
-        test2.appendChild(this.#span1)
-
-
+    }
+    render() {
+        this.#div1.appendChild(this.#div2)
+        this.#div2.appendChild(this.#span1)
+        let test2 = document.querySelector("body > div.page-root.page-root_no-player.deco-pane-back.theme.theme_dark.black > div.bar > div.bar__content > div.player-controls.deco-player-controls > div.player-controls__seq-controls")
+        test2.appendChild(this.#div1)
     }
 }
 
 
 
 setTimeout(() => {
-    new Test()
+    new Test().render();
 }, 250)
