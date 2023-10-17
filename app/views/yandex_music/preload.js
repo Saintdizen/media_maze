e7ce1bc060dd284406aee798e5e60071fb798ebd
syzoth <saintdizen@gmail.com>
2023-10-17 14:16:46 +0300

    0.0.9

--- app/views/yandex_music/preload.js
+++ app/views/yandex_music/preload.js
@@ -1,15 +1,17 @@
 //Mu.blocks.di.repo.player.play();
-class Test {
-    #div1 = document.createElement("div");
-    #div2 = document.createElement("div");
-    #span1 = document.createElement("span");
-    constructor() {
-        this.#div1.className = "hq";
-        this.#div2.className = "hq__icon player-controls__btn deco-player-controls__button"
-        this.#span1.className = "d-icon deco-icon d-icon_share"
-        this.#span1.style.rotate = "180deg";
-        this.#span1.id = "test111"
-        this.#div1.addEventListener("click", async (evt) => {
+
+function render() {
+    if (document.getElementById("TESTTT") === null) {
+        let div1 = document.createElement("div");
+        div1.id = "TESTTT"
+        let div2 = document.createElement("div");
+        let span1 = document.createElement("span");
+        div1.className = "hq";
+        div2.className = "hq__icon player-controls__btn deco-player-controls__button"
+        span1.className = "d-icon deco-icon d-icon_share"
+        span1.style.rotate = "180deg";
+        span1.id = "test111"
+        div1.addEventListener("click", async (evt) => {
             let xhr = new XMLHttpRequest();
             xhr.open("GET", Mu.blocks.di.repo.player.getTrack()._$f9);
             xhr.responseType = "arraybuffer";
@@ -18,7 +20,7 @@ class Test {
                     let artist = Mu.blocks.di.repo.player.getTrack().artists[0].name;
                     let title = Mu.blocks.di.repo.player.getTrack().title;
                     let link = document.createElement("a");
-                    let blob = new Blob([xhr.response], {type: "application/pdf"});
+                    let blob = new Blob([xhr.response], {type: "audio/mp3"});
                     link.href = URL.createObjectURL(blob);
                     link.download = `${artist} - ${title}.mp3`;
                     link.target = '_blank'
@@ -29,13 +31,12 @@ class Test {
                 }
             };
             xhr.send();
-        })
-    }
-    render() {
-        this.#div1.appendChild(this.#div2)
-        this.#div2.appendChild(this.#span1)
+        });
+        div1.appendChild(div2)
+        div2.appendChild(span1)
         let test2 = document.querySelector("body > div.page-root.page-root_no-player.deco-pane-back.theme.theme_dark.black > div.bar > div.bar__content > div.player-controls.deco-player-controls > div.player-controls__seq-controls")
-        test2.appendChild(this.#div1)
+        test2.appendChild(div1)
     }
 }
-setTimeout(() => new Test().render(), 250)
+
+setTimeout(() => render(), 250)
