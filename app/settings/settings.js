f9a3cef61dc93616ca5d1e29dc3410a40314222a
syzoth <saintdizen@gmail.com>
2023-10-30 16:45:59 +0300

    0.1.6

--- app/settings/settings.js
+++ app/settings/settings.js
@@ -6,70 +6,43 @@ class Settings {
         direction: Styles.DIRECTION.COLUMN, wrap: Styles.WRAP.NOWRAP,
         align: Styles.ALIGN.CENTER, justify: Styles.JUSTIFY.CENTER
     })
+    #elements = new SettingsElements();
     constructor() {
         this.#main.add(
-            this.setPlaybackBlock(),
-            this.setTracksBlock(),
-            this.setInterfaceBlock()
+            this.#elements.setFieldSet("Интерфейс", [
+                this.#elements.setCheckBox("Новый интерфейс (Требуется перезапуск)", SettingsMarks.INTERFACE.new_skin)
+            ]),
+            this.#elements.setFieldSet("Музыка", [
+                this.#elements.setCheckBox("Загрузка музыки (Требуется перезапуск)", SettingsMarks.TRACKS.download)
+            ]),
+            this.#elements.setFieldSet("Воспроизведение", [
+                this.#elements.setCheckBox("Автоматическое воспроизведение музыки", SettingsMarks.PLAYBACK.autoplay)
+            ])
         )
         return this.#main;
     }
-    setInterfaceBlock() {
-        // === Автоматическое воспроизведение музыки ===
-        let new_skin = new CheckBox({title: "Новый интерфейс (Требуется перезапуск)"})
-        new_skin.setValue(store.get(SettingsMarks.INTERFACE.new_skin))
-        new_skin.addChangeListener((e) => {
-            store.set(SettingsMarks.INTERFACE.new_skin, e.target.checked)
-            new Notification({ title: new_skin.getTitle(), text: `Сохранено`, style: Notification.STYLE.SUCCESS, showTime: 1000 }).show()
-        })
-        // ===
+}
+
+class SettingsElements {
+    setFieldSet(title = String(), components = []) {
         return new FieldSet({
-            title: "Музыка",
+            title: title,
             style: {
                 direction: Styles.DIRECTION.COLUMN, wrap: Styles.WRAP.NOWRAP,
                 align: Styles.ALIGN.CENTER, justify: Styles.JUSTIFY.CENTER,
                 width: Styles.SIZE.WEBKIT_FILL
             },
-            components: [new_skin]
+            components: components
         })
     }
-    setTracksBlock() {
-        // === Автоматическое воспроизведение музыки ===
-        let download_track = new CheckBox({title: "Загрузка музыки (Требуется перезапуск)"})
-        download_track.setValue(store.get(SettingsMarks.TRACKS.download))
-        download_track.addChangeListener((e) => {
-            store.set(SettingsMarks.TRACKS.download, e.target.checked)
-            new Notification({ title: download_track.getTitle(), text: `Сохранено`, style: Notification.STYLE.SUCCESS, showTime: 1000 }).show()
+    setCheckBox(title = String(), stores = String()) {
+        let checkBox = new CheckBox({title: title})
+        checkBox.setValue(store.get(stores))
+        checkBox.addChangeListener((e) => {
+            store.set(stores, e.target.checked)
+            new Notification({ title: checkBox.getTitle(), text: `Сохранено`, style: Notification.STYLE.SUCCESS, showTime: 1000 }).show()
         })
-        // ===
-        return new FieldSet({
-            title: "Музыка",
-            style: {
-                direction: Styles.DIRECTION.COLUMN, wrap: Styles.WRAP.NOWRAP,
-                align: Styles.ALIGN.CENTER, justify: Styles.JUSTIFY.CENTER,
-                width: Styles.SIZE.WEBKIT_FILL
-            },
-            components: [download_track]
-        })
-    }
-    setPlaybackBlock() {
-        // === Автоматическое воспроизведение музыки ===
-        let autoplay = new CheckBox({title: "Автоматическое воспроизведение музыки"})
-        autoplay.setValue(store.get(SettingsMarks.PLAYBACK.autoplay))
-        autoplay.addChangeListener((e) => {
-            store.set(SettingsMarks.PLAYBACK.autoplay, e.target.checked)
-            new Notification({ title: autoplay.getTitle(), text: `Сохранено`, style: Notification.STYLE.SUCCESS, showTime: 1000 }).show()
-        })
-        // ===
-        return new FieldSet({
-            title: "Воспроизведение",
-            style: {
-                direction: Styles.DIRECTION.COLUMN, wrap: Styles.WRAP.NOWRAP,
-                align: Styles.ALIGN.CENTER, justify: Styles.JUSTIFY.CENTER,
-                width: Styles.SIZE.WEBKIT_FILL
-            },
-            components: [autoplay]
-        })
+        return checkBox
     }
 }
 
