9de971e9510674e497c20db899e02cd23d19bb7a
syzoth <saintdizen@gmail.com>
2024-01-12 13:30:34 +0300

    0.3.7

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-const {AppLayout, render, Log, Icons, Styles} = require('chuijs');
+const {AppLayout, render, Log, Icons, Styles, Route} = require('chuijs');
 const {Settings} = require("./settings/settings");
 
 const {YandexMusicPage} = require('./views/yandex_music/yandex_music')
@@ -9,9 +9,11 @@ class App extends AppLayout {
         super();
         //this.setSearchToAppMenu();
         this.setAutoCloseRouteMenu();
-        //this.disableAppMenu()
-        this.setRoute(new YandexMusicPage());
-        this.setRoute(new OfflinePlayer());
+        this.disableAppMenu()
+
+        let ya_page = new YandexMusicPage()
+        let off_page = new OfflinePlayer()
+        this.setRoute(ya_page);
 
         this.addToHeader([
             AppLayout.DIALOG({
@@ -32,6 +34,20 @@ class App extends AppLayout {
                     components: [ new Settings() ]
                 }
             }),
+            AppLayout.BUTTON({
+                    title: "Оффлайн проигрыватель",
+                    icon: Icons.FILE.DOWNLOAD_FOR_OFFLINE,
+                    reverse: true,
+                    clickEvent: () => new Route().go(off_page)
+                }
+            ),
+            AppLayout.BUTTON({
+                    title: "Музыка",
+                    icon: Icons.AUDIO_VIDEO.LIBRARY_MUSIC,
+                    reverse: true,
+                    clickEvent: () => new Route().go(ya_page)
+                }
+            ),
         ])
     }
 }
